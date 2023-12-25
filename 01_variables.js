11337efe855cbf3201fa54c0909827c953757ab5
Arijit Laha <mearijitlaha@gmail.com>
2023-12-26 01:30:05 +0530

    1st day

--- 01_variables.js
+++ 01_variables.js
@@ -1,4 +1,15 @@
-const accountId = 144553
-let accountEmail = "mearijitlaha@gmail.com"
-var accountPassword = "12345"
-accountCity = "Howrah"
+const accountId = 144553;
+let accountEmail = "randomEmail1@gmail.com";
+var accountPassword = "12345";
+accountCity = "Howrah";
+accountEmail = "randomEmail2@gmail.com";
+accountPassword = "54321";
+accountCity = "Kolkata";
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+Prefer not to use var
+becauseof issue in block scope and functional scope
+*/
